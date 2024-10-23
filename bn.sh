c90533ce7a1a21c0b83c2b5aa428635cf8d6b938
behzadjanjua <janjuab@mcmaster.ca>
2024-10-23 13:38:22 -0400

    "Forgot a >&2"

--- bn.sh
+++ bn.sh
@@ -60,7 +60,7 @@ fi
 # Validate gender format using regex; if invalid, print an error and exit
 if ! [[ "$gender" =~ $gender_regex ]]; 
 then
-    echo "Badly formatted assigned gender: $gender" 
+    echo "Badly formatted assigned gender: $gender">&2 
     echo "Usage: ./bn.sh <year> <gender f|F|m|M|b|B>" >&2
     usage
     exit 2
